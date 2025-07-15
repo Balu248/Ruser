fe6ab9e51cb5bbb755399b09ffe96ecd4f746c01
Balu248 <balachandran7236@gmail.com>
2025-07-15 09:14:39 +0530

    User api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-secondary">
+            <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">User App👤</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
